b2c122aef0623b8fe0e7f953048a9966ce07e128
Bohdan <booking.bogdan@gmail.com>
2024-11-09 00:40:02 +0200

    inf cycle add

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ bot = telebot.TeleBot('7557624575:AAEEj79BO0T5y505m3rQcRjIZfM8Pc9ocYc')
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, 'Привіт! Я бот, який надає прогноз погоди')
+
+bot.polling(none_stop=True)
