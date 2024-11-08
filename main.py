ad93ee14bf002739a4272b7a9c36758b2ca1e71f
Bohdan <booking.bogdan@gmail.com>
2024-11-09 00:10:52 +0200

    decarators message add

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import telebot
 
 bot = telebot.TeleBot('7557624575:AAEEj79BO0T5y505m3rQcRjIZfM8Pc9ocYc')
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Привіт! Я бот, який надає прогноз погоди')
