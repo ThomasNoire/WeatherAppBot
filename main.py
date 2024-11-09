3005f15a7bde159dc2d0ec0eb909d862d6bcdcdc
Bohdan <booking.bogdan@gmail.com>
2024-11-09 13:16:14 +0200

    create APi KEY openweathermap

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import telebot
 
 bot = telebot.TeleBot('7557624575:AAEEj79BO0T5y505m3rQcRjIZfM8Pc9ocYc')
+API = '4ced331457796d67ffddddce9ccb0190'
 
 @bot.message_handler(commands=['start'])
-def main(message):
+def start(message):
     bot.send_message(message.chat.id, 'Привіт! Я бот, який надає прогноз погоди')
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+ city = message.text.strip().lower()
+
 
 bot.polling(none_stop=True)
+
+# if __name__ == '__main__':
+#     bot.infinity_polling()
