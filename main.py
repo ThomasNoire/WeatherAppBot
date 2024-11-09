f31d94eb94c806388658795da9f2059a24e1585d
Bohdan <booking.bogdan@gmail.com>
2024-11-09 21:20:00 +0200

    fix: save city input and validate before forecast retrieval

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-# import telebot
-# import requests
-# import json
-#
-# bot = telebot.TeleBot('7557624575:AAEEj79BO0T5y505m3rQcRjIZfM8Pc9ocYc')
-# API = '4ced331457796d67ffddddce9ccb0190'
-#
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#     bot.send_message(message.chat.id, 'Привіт! Я бот, який надає прогноз погоди. Будьласка вкажи місто (або населений пункт)')
-#
-#
-# @bot.message_handler(content_types=['text'])
-# def get_weather(message):
-#      city = message.text.strip().lower()
-#      res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
-#      data = json.loads(res.text)
-#      bot.reply_to(message, f'Погода сьогодні: {data["main"]["temp"]}')
-#
-# bot.polling(none_stop=True)
-
 import telebot
 import requests
 from telebot import types
@@ -29,6 +8,7 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast"
 
 bot = telebot.TeleBot(API_TOKEN)
+user_city = {}
 
 def get_weather(city):
     params = {
@@ -40,31 +20,31 @@ def get_weather(city):
     response = requests.get(BASE_URL, params=params)
     return response.json()
 
-
 def get_forecast(city):
     params = {
         'q': city,
         'appid': API_KEY,
-        'units': 'metric',  # Температура в градусах Цельсія
-        'lang': 'uk'  # Мова відповіді
+        'units': 'metric',
+        'lang': 'uk'
     }
     response = requests.get(FORECAST_URL, params=params)
     return response.json()
-
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Привіт! Напишіть назву міста, щоб дізнатися прогноз погоди.")
 
-
-@bot.message_handler(func=lambda message: True)
-def send_weather(message):
+@bot.message_handler(func=lambda message: message.text not in ["Прогноз на завтра", "Прогноз на наступні 5 днів"])
+def handle_city_input(message):
     city = message.text
     weather_data = get_weather(city)
 
     if weather_data.get('cod') != 200:
         bot.reply_to(message, f"Не вдалося знайти місто: {city}. Спробуйте ще раз.")
     else:
+
+        user_city[message.chat.id] = city
+
         temp = weather_data['main']['temp']
         description = weather_data['weather'][0]['description']
         temp_min = weather_data['main']['temp_min']
@@ -75,31 +55,45 @@ def send_weather(message):
                        f"Опис: {description}\n" \
                        f"Температура від {temp_min}°C до {temp_max}°C"
 
-        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
         item1 = types.KeyboardButton("Прогноз на завтра")
         item2 = types.KeyboardButton("Прогноз на наступні 5 днів")
         markup.add(item1, item2)
 
         bot.send_message(message.chat.id, weather_info, reply_markup=markup)
+
 @bot.message_handler(func=lambda message: message.text == "Прогноз на завтра")
 def send_tomorrow_forecast(message):
-    city = message.text
-    forecast_data = get_forecast(city)
+    city = user_city.get(message.chat.id)
+    if not city:
+        bot.reply_to(message, "Будь ласка, спочатку введіть місто.")
+        return
 
-    tomorrow_forecast = forecast_data['list'][1]
+    forecast_data = get_forecast(city)
+    if forecast_data.get('cod') != '200':
+        bot.reply_to(message, "Не вдалося отримати прогноз. Спробуйте ще раз пізніше.")
+        return
+
+    tomorrow_forecast = forecast_data['list'][8]
     temp = tomorrow_forecast['main']['temp']
     description = tomorrow_forecast['weather'][0]['description']
 
-    forecast_info = f"Прогноз на завтра:\nТемпература: {temp}°C\nОпис: {description}"
+    forecast_info = f"Прогноз на завтра в місті {city}:\nТемпература: {temp}°C\nОпис: {description}"
     bot.send_message(message.chat.id, forecast_info)
-
 
 @bot.message_handler(func=lambda message: message.text == "Прогноз на наступні 5 днів")
 def send_5day_forecast(message):
-    city = message.text
-    forecast_data = get_forecast(city)
+    city = user_city.get(message.chat.id)
+    if not city:
+        bot.reply_to(message, "Будь ласка, спочатку введіть місто.")
+        return
 
-    forecast_info = "Прогноз на наступні 5 днів:\n"
+    forecast_data = get_forecast(city)
+    if forecast_data.get('cod') != '200':
+        bot.reply_to(message, "Не вдалося отримати прогноз. Спробуйте ще раз пізніше.")
+        return
+
+    forecast_info = f"Прогноз на наступні 5 днів в місті {city}:\n"
     for i in range(0, 40, 8):
         day_forecast = forecast_data['list'][i]
         temp = day_forecast['main']['temp']
@@ -110,6 +104,8 @@ def send_5day_forecast(message):
 
     bot.send_message(message.chat.id, forecast_info)
 
-
 bot.polling(none_stop=True)
+
+
+
 
